a4303031e86827e9e7386a6fba89e54ae1dbe5b2
Louvivien <vivien.richaud@gmail.com>
2018-10-18 21:31:33 +0200

    modif fichier app

--- app.rb
+++ app.rb
@@ -3,6 +3,17 @@ Bundler.require
 
 $LOAD_PATH.unshift File.expand_path('lib/app', __dir__)
 require 'townhalls_mailer'
+require 'townhalls_scraper'
+require 'townhalls_adder_to_db'	
+require 'townhalls_follower'
 
-# on appelle notre classe mailer qui va aller envoyer des emails à partir du fichier CSV
+
+
+# on appelle notre classe ScrappeRegion qui va aller scraper les emails et les mettre dans un fichier JSON
+ScrappeRegion.new.perform
+
+
+
+# on appelle notre classe Mailer qui va aller envoyer des emails à partir du fichier JSON
 Mailer.new.browse_json
+
