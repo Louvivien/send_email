463a1d4a8f076c484944ffe6978c9e63d2f1962d
Louvivien <vivien.richaud@gmail.com>
2018-10-18 20:58:13 +0200

    final version

--- lib/app/townhalls_mailer.rb
+++ lib/app/townhalls_mailer.rb
@@ -7,9 +7,9 @@ require 'dotenv'
 Dotenv.load
 
 class Mailer
-  # cette méthode envoie les mails avec pour entrée deux élements les emails et les communes
-  def send_emails(email, commune)
-    adresse = email
+  # cette méthode envoie les mails avec pour entrée deux élements les mails et les names
+  def send_mails(mail, name)
+    adresse = mail
 
     gmail = Gmail.connect('patti.lala4@gmail.com', 'rubythpgmail')
     gmail.deliver do
@@ -17,8 +17,8 @@ class Mailer
       subject "Le projet THP propose d'apprendre à vos administrés d'apprendre gratuitement à programmer !"
       content_type 'text/html; charset=UTF-8'
       body "<p>Bonjour, <br><br>
-  Je m'appelle Patricia Laroche, je suis élève à The Hacking Project, une formation au code gratuite, sans locaux, sans sélection, sans restriction géographique. La pédagogie de notre école est celle du peer-learning, où nous travaillons par petits groupes sur des projets concrets qui font apprendre le code. Le projet du jour est d'envoyer (avec du codage) des emails aux mairies pour qu'ils nous aident à faire de The Hacking Project un nouveau format d'éducation pour tous.<br><br>
-  Déjà 500 personnes sont passées par <a href='https://www.thehackingproject.org/' target='_blank'>The Hacking Project</a>. Est-ce que la mairie de #{commune} veut changer le monde avec nous ?<br><br>
+  Je m'appelle Patricia Laroche, je suis élève à The Hacking Project, une formation au code gratuite, sans locaux, sans sélection, sans restriction géographique. La pédagogie de notre école est celle du peer-learning, où nous travaillons par petits groupes sur des projets concrets qui font apprendre le code. Le projet du jour est d'envoyer (avec du codage) des mails aux mairies pour qu'ils nous aident à faire de The Hacking Project un nouveau format d'éducation pour tous.<br><br>
+  Déjà 500 personnes sont passées par <a href='https://www.thehackingproject.org/' target='_blank'>The Hacking Project</a>. Est-ce que la mairie de #{name} veut changer le monde avec nous ?<br><br>
   Charles, co-fondateur de The Hacking Project pourra répondre à toutes vos questions : 06.95.46.60.80</p>"
     end
     gmail.logout
@@ -29,11 +29,11 @@ class Mailer
     file = File.read('./db/townhalls.json')
     datas = JSON.parse(file)
 
-    # elle fait une boucle dnas le fichier json pour sortir deux eléments, les emails et les communes
+    # elle fait une boucle dnas le fichier json pour sortir deux eléments, les mails et les names
     (0...datas.length).each do |i|
-      send_emails(datas[i]['email'], datas[i]['commune'])
-      puts datas[i]['commune']
-      puts datas[i]['email']
+      send_mails(datas[i]['mail'], datas[i]['name'])
+      puts datas[i]['name']
+      puts datas[i]['mail']
     end
   end
 end
